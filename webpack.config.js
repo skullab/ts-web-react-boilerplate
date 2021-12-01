e24781506e390c531b882856571b785e7a40d7d2
ivan.maruca <ivan.maruca@gmail.com>
2021-12-01 23:18:28 +0100

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+const config = {
+  entry: './src/index.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+};
+
+module.exports = (env,args) => {
+    return [config]
+};
